33bd45bf0855e71dfdf6e1e3f538ac934c06cbd4
Marcel Ndeffo <marcel@aampe.com>
2022-09-09 08:53:27 +0200

    remove secrets from repo

--- src/translator.py
+++ src/translator.py
@@ -1,32 +1,22 @@
 """Translator"""
 
-# IMPORT DEPS
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-#from logging import getLogger, DEBUG
+from decouple import config
 
-#logger = getLogger('ibm-translator-project')
-#logger.setLevel(DEBUG)
+APIKEY = config('APIKEY')
+URL = config('URL')
 
-
-# AUTHENTICATE
-APIKEY = 'l28JkJlJCJeRdDtPaJcAttI8uIrkqofZxr7VNhAU66FZ'
-URL = 'https://api.us-south.language-translator.watson.cloud.ibm.com'
-'/instances/63ce5f51-902a-4b00-9086-4a3d44387f0a'
-
-# SETUP SERVICES
-AUTHENTIFICATOR = IAMAuthenticator(APIKEY)
-LT = LanguageTranslatorV3(version='2018-05-01', authenticator=AUTHENTIFICATOR)
+AUTHENTICATOR = IAMAuthenticator(APIKEY)
+LT = LanguageTranslatorV3(version='2018-05-01', authenticator=AUTHENTICATOR)
 LT.set_service_url(URL)
 
 
-# ENGLISH TO FRENCH TRANSLATION FUNCTION
 def english_to_french(text):
-    """translates english to french"""
+    """translates english to French"""
     LT.set_service_url(URL)
     translate_en_fr = LT.translate(text=text, model_id='en-fr').get_result\
         ()['translations'][0]['translation']
-    #logger.debug(f"input test: {text}, output text: {translate_en_fr}")
     return translate_en_fr
 
 
@@ -36,5 +26,4 @@ def english_to_german(text):
     LT.set_service_url(URL)
     translate_en_de = LT.translate(text=text, model_id='en-de').get_result\
         ()['translations'][0]['translation']
-    #logger.debug(f"input test: {text}, output text: {translate_en_de}")
     return translate_en_de
--- tests/test_tranlator.py
+++ tests/test_tranlator.py
@@ -1,4 +1,4 @@
-# IMPORT UNITTEST
+"""unitest translator"""
 import unittest
 
 from ibm_cloud_sdk_core import ApiException
@@ -6,32 +6,42 @@ from src.translator import english_to_french, english_to_german
 
 
 class TestEnglishToFrench(unittest.TestCase):
+    """test english to French translation"""
     def test_hello(self):
+        """test hello word"""
         self.assertEqual('Bonjour', english_to_french('hello'))
 
     def test_verb(self):
+        """test a verb"""
         self.assertEqual('à manger', english_to_french('to eat'))
 
     def test_dummy(self):
+        """test if not translate whatever"""
         self.assertNotEqual('il fait beau', english_to_french('how are you'))
 
     def test_empty(self):
+        """test if api error for bad text"""
         with self.assertRaises(ApiException):
             english_to_french('')
 
 
 class TestEnglishToGerman(unittest.TestCase):
+    """test english to german translation"""
 
     def test_hello(self):
+        """test hello word"""
         self.assertEqual('Hallo', english_to_german('hello'))
 
     def test_verb(self):
+        """test a verb"""
         self.assertEqual('zu essen', english_to_german('to eat'))
 
     def test_dummy(self):
+        """test if not translate whatever"""
         self.assertNotEqual('es ist sonnig', english_to_german('how are you'))
 
     def test_empty(self):
+        """test if api error for bad text"""
         with self.assertRaises(ApiException):
             english_to_german('')
 
